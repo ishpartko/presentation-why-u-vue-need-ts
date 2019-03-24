f9bf7589586f64b90a1dfd23cc3edd6ebdb6b19c
ishpartko <ishpartko@gmail.com>
2019-03-24 13:28:43 +0300

    update presentation

--- Example.ts
+++ Example.ts
@@ -1,27 +1,7 @@
-interface IUser {
-  id: number;
+function calculateDate(format: boolean, value: number, method: string): Date {
+    let date = new Date();
+    /* business logic */
+    return date;
 }
 
-interface IAdmin extends IUser {
-  requestBan(id: number): void;
-}
-
-class User implements IUser {
-  id: number;
-}
-
-class Admin implements IAdmin {
-  id: number;
-  requestBan(id: number) {
-    console.log('ОК, забаню ' + id);
-  }
-}
-
-const user1 = new User();
-const user2 = new Admin();
-
-user1.requestBan();
-
-user2.requestBan('123');
-
-const any = 1;
+calculateDate('true', 123, 'method_1');
