156c00e40b00295ac8ef8b439226a1e63b3f28b7
ishpartko <ishpartko@gmail.com>
2019-03-30 01:43:22 +0300

    update

---
+++ Example.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+/** 
+  @function Determines some car
+  @param {boolean} rightHand - It has right hand drive
+  @param {string} model - Model of new Car
+  @param {number} year - year of car
+*/
+function createCar(rightHand, model, year) {
+  const car = {
+    rightHand,
+    model,
+    year
+  }
+  return car;
+}
+
+createCar('True', 'DonetskJS car', 2019);
